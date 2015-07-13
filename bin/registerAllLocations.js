fa079e393f4044bbf99da78d8c86c01accad89ba
albertosouza <alberto.souza.99@gmail.com>
2015-07-13 02:29:04 -0300

    add contry list

--- bin/registerAllLocations.js
+++ bin/registerAllLocations.js
@@ -6,6 +6,7 @@
 
 // TODO add suport to load others locations from .json
 var locations = require('../lib/locations/BR.json');
+var countries = require('../lib/locations/countries.json');
 var async = require('async');
 
 var CLI = {};
@@ -15,45 +16,45 @@ CLI.saveLocations = function saveLocations(we, done) {
   var stateCount = 0;
   console.time('State and city creation time:');
 
-  return we.db.models.lcountry.create({
-    name: 'Brasil',
-    code: 'BR',
-  }).then(function (country) {
-    return async.each(locations.states, function(state, nextState) {
-      return we.db.models.lstate.create({
-        code: state.code,
-        name: state.name,
-        countryId: country.id
-      }).then(function (salvedState) {
-        return async.each(state.cities, function (city, nextCity) {
-          return we.db.models.lcity.create({
-            name: city,
-            stateId: salvedState.id
-          }).then(function () {
-            cityCount++;
-             return nextCity();
-          }).catch(nextCity);
-        }, function afterCreatedAllCities (err) {
-          if (err) {
-            we.log.error('Error on create cities',err);
-            return done(err);
-          }
+  return we.db.models.lcountry.bulkCreate(countries).then(function (country) {
 
-          stateCount++;
-          return nextState();
-        });
+    we.db.models.lcountry.findOne({where: { code: 'BR' } }).then(function(country) {
+      return async.each(locations.states, function(state, nextState) {
+        return we.db.models.lstate.create({
+          code: state.code,
+          name: state.name,
+          countryId: country.id
+        }).then(function (salvedState) {
+          return async.each(state.cities, function (city, nextCity) {
+            return we.db.models.lcity.create({
+              name: city,
+              stateId: salvedState.id
+            }).then(function () {
+              cityCount++;
+               return nextCity();
+            }).catch(nextCity);
+          }, function afterCreatedAllCities (err) {
+            if (err) {
+              we.log.error('Error on create cities',err);
+              return done(err);
+            }
 
-      }).catch(nextState);
-    },function afterCreateStates(err) {
-      if (err) {
-        we.log.error('Error on create states', err);
-      }
+            stateCount++;
+            return nextState();
+          });
 
-      we.log.info('DONE created '+ stateCount + ' states and ' + cityCount + ' cities');
-      console.timeEnd('State and city creation time:');
+        }).catch(nextState);
+      },function afterCreateStates(err) {
+        if (err) {
+          we.log.error('Error on create states', err);
+        }
 
-      return done();
-    })
+        we.log.info('DONE created '+ stateCount + ' states and ' + cityCount + ' cities');
+        console.timeEnd('State and city creation time:');
+
+        return done();
+      })
+    }).catch(done);
   }).catch(done);
 }
 
